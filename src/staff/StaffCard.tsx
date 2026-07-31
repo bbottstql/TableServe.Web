fc25829fbe4435b8413ec03788b6e88729ebb1ed
Bradley Botts <BBotts@tql.com>
2026-07-31 11:32:35 -0400

    fixed staffcard to look better

--- src/staff/StaffCard.tsx
+++ src/staff/StaffCard.tsx
@@ -12,51 +12,45 @@ interface IStaffCardProps {
 
 function StaffCard({ staff, onRemove }: IStaffCardProps) {
   return (
-    
-    <div className="card p-4" style={{ width: "23rem" }} key={staff.id}>
-       
-      <span className="fs-4 fw-medium d-flex">
-        {staff.firstName} {staff.lastName}
-        <Dropdown className="d-flex ms-4 ps-5">
-        <Dropdown.Toggle
-          className="btn btn-light"
-          style={{ background: "none" }}
-        >
-          <svg className="bi pe-none" width={20} height={20} fill="#007AFF">
-            <use xlinkHref={`${bootstrapIcons}#three-dots-vertical`} />
-          </svg>
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-          <Dropdown.Item as={Link} to={`/staff/edit/${staff.id}`}>
-            Edit
-          </Dropdown.Item>
-          <Dropdown.Item
-            as="a"
-            href="#"
-            onClick={async (event) => {
+       <div className="card p-4" style={{ width: '23rem' }}>
+      <div className="d-flex justify-content-end">
+        <Dropdown className="d-inline">
+          <Dropdown.Toggle className="btn btn-light no-caret" style={{ background: 'none' }}>
+            <svg className="bi pe-none" width={20} height={20} fill="#007AFF">
+              <use xlinkHref={`${bootstrapIcons}#three-dots-vertical`} />
+            </svg>
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item as={Link} to={`/staff/edit/${staff.id}`}>Edit</Dropdown.Item>
+            <Dropdown.Item as="a" href="#" onClick={async (event) => {
               event.preventDefault();
-              if (confirm("Delete this staff member?") && staff.id) {
+              if (confirm('Delete this staff member?') && staff.id) {
                 await staffAPI.delete(staff.id);
                 onRemove(staff);
               }
-            }}
-          >
-            Delete
-          </Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
+            }}>Delete</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+ 
+      <span className="fs-4 fw-medium">
+        {staff.firstName} {staff.lastName}
       </span>
-      
-      <span className="text-secondary">Username: {staff.username}</span>
+ 
       <span className="text-secondary">
-        Phone: {staff.phone && formatPhoneNumber(staff.phone)}
+        {staff.username}
+        <br />
+        {formatPhoneNumber(staff.phone)}
+        <br />
+        {staff.email}
+ 
+        <br />
+ 
+        <span className="d-flex gap-2 mt-2">
+          {staff.isManager && <span className="badge text-bg-primary mt-1">Manager</span>}{' '}
+          {staff.isAdmin && <span className="badge text-bg-dark mt-1">Admin</span>}
+        </span>
       </span>
-      <span className="text-secondary">Email: {staff.email}</span>
-      {staff.isManager && (
-        <span className="badge text-bg-primary">Manager</span>
-      )}
-      {staff.isAdmin && <span className="badge text-bg-dark">Admin</span>}
-      
     </div>
   );
 }
